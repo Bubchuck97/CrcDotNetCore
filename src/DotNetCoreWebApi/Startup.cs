5dea30c1f14832cda680b8d204d3f5fa12d71405
mateusz <mateuszleki.7@gmail.com>
2019-04-10 17:31:30 +0200

    added swagger

--- src/DotNetCoreWebApi/Startup.cs
+++ src/DotNetCoreWebApi/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace DotNetCoreWebApi
 {
@@ -34,6 +35,15 @@ namespace DotNetCoreWebApi
             services.AddDbContext<MeasurementContext>(options => options.UseSqlServer(Configuration["ConnectionString:LabDb"]));
 
             services.AddScoped<IMeasurementRepository<Measurement>, MeasurementRepository>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "WebApiServer API",
+                    Version = "v1"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +61,12 @@ namespace DotNetCoreWebApi
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApiServer API V!");
+            });
         }
     }
 }
