36b862d9b9350e065a9c8f4cd752812ea8facdb0
mateusz <mateuszleki.7@gmail.com>
2019-04-24 18:07:24 +0200

    added removing element from list

--- src/DotNetCoreWebClient/wwwroot/js/measurementListSection.js
+++ src/DotNetCoreWebClient/wwwroot/js/measurementListSection.js
@@ -3,11 +3,16 @@
         let grid = document.querySelector('#measurements_container')
         let newRow = grid.children[1].cloneNode(true)
 
-        let nameColumn = newRow.querySelector('div[data-columnt-type=\'name\']')
-        let valueColumn = newRow.querySelector('div[data-columnt-type=\'value\']')
+        let nameColumn = newRow.querySelector('div[data-column-type=\'name\']')
+        let valueColumn = newRow.querySelector('div[data-column-type=\'value\']')
+        let removeBtn = newRow.querySelector('a[data-action=\'remove\']')
 
         nameColumn.innerHTML = measurement.name
         valueColumn.innerHTML = measurement.value
+        removeBtn.addEventListener('click', e => {
+            //removeBtn.parentElement.parentElement.remove()
+            newRow.remove()
+        })
 
 
         newRow.classList.remove('d-none')
